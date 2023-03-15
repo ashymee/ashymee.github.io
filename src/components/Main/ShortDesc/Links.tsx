8a5203ece99a130685e3da8c1e29dbe858188caf
ashymee <ashymee@gmail.com>
2023-03-16 00:54:59 +0700

    feat: update packages, restyling & add filtering button

--- src/components/Main/ShortDesc/Links.tsx
+++ src/components/Main/ShortDesc/Links.tsx
@@ -1,37 +1,43 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import {
   FaEnvelopeSquare,
   FaFacebookSquare,
   FaGithubSquare,
+  FaGlobe,
   FaInstagramSquare,
   FaLinkedin,
-} from 'react-icons/fa';
+} from "react-icons/fa";
 
 const links = [
   {
-    name: 'Github',
+    name: "Blog",
+    comp: <FaGlobe />,
+    url: "https://devstreetlab.my.id",
+  },
+  {
+    name: "Github",
     comp: <FaGithubSquare />,
-    url: 'https://github.com/ashymee',
+    url: "https://github.com/ashymee",
   },
   {
-    name: 'LinkedIn',
+    name: "LinkedIn",
     comp: <FaLinkedin />,
-    url: 'https://www.linkedin.com/in/aswinhimawan/',
+    url: "https://www.linkedin.com/in/aswinhimawan/",
   },
   {
-    name: 'Instagram',
+    name: "Instagram",
     comp: <FaInstagramSquare />,
-    url: 'https://www.instagram.com/ashymeeid',
+    url: "https://www.instagram.com/ashymeeid",
   },
   {
-    name: 'Facebook',
+    name: "Facebook",
     comp: <FaFacebookSquare />,
-    url: 'https://www.facebook.com/aswin.himawan.sahlan',
+    url: "https://www.facebook.com/aswin.himawan.sahlan",
   },
   {
-    name: 'Mail',
+    name: "Mail",
     comp: <FaEnvelopeSquare />,
-    url: 'mailto:ashymee@gmail.com',
+    url: "mailto:ashymee@gmail.com",
   },
 ];
 
@@ -39,17 +45,18 @@ const Links = () => {
   const { t } = useTranslation();
 
   return (
-    <div className=''>
-      <div className='divider'>{t('tautan')}</div>
-      <div className='flex flex-wrap justify-center space-x-2'>
+    <div className="">
+      <div className="divider">{t("koneksi")}</div>
+      <div className="flex flex-wrap justify-center space-x-2">
         {links.map((link, index) => (
           <a
-            className='btn btn-square text-2xl'
+            className="btn btn-square text-2xl"
             key={link.name}
             href={link.url}
-            target='_blank'
+            target="_blank"
             title={link.name}
-            rel='noopener noreferrer nofollow'>
+            rel="noopener noreferrer nofollow"
+          >
             {link.comp}
           </a>
         ))}
--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,4 +1,9 @@
+export type SkillProps = "all" | "pemula" | "menengah" | "mahir";
+
 export type UseStoreProps = {
   lang: string;
   setLang: (lang: string) => void;
+
+  filteredSkill: SkillProps;
+  setFilteredSkill: (filteredSkill: SkillProps) => void;
 };
