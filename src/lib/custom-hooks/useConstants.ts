f890064cb61ef0ac837def0559b6b3bdc9f55302
Aswin Himawan <ashymee@gmail.com>
2024-08-01 01:03:17 +0700

    chore: remove trailing slash

--- src/lib/custom-hooks/useConstants.ts
+++ src/lib/custom-hooks/useConstants.ts
@@ -11,12 +11,14 @@ const useConstants = () => {
   const proj = "/data/projects.json";
   const bio = "/data/short-bio.json";
 
-  const json_certification = isDev ? `${URL}/${cert}` : cert;
-  const json_contact = isDev ? `${URL}/${contact}` : contact;
-  const json_education = isDev ? `${URL}/${edu}` : edu;
-  const json_experiences = isDev ? `${URL}/${exp}` : exp;
-  const json_projects = isDev ? `${URL}/${proj}` : proj;
-  const json_bio = isDev ? `${URL}/${bio}` : bio;
+  const json_certification = isDev ? `${URL}${cert}` : cert;
+  const json_contact = isDev ? `${URL}${contact}` : contact;
+  const json_education = isDev ? `${URL}${edu}` : edu;
+  const json_experiences = isDev ? `${URL}${exp}` : exp;
+  const json_projects = isDev ? `${URL}${proj}` : proj;
+  const json_bio = isDev ? `${URL}${bio}` : bio;
+
+  console.log({ json_contact });
 
   return {
     URL,
